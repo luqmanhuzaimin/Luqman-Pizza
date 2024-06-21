686cbac426d3f59d3a1fef46908f527aec579c6c
luqmanhuzaimin <54978009+luqmanhuzaimin@users.noreply.github.com>
2024-06-21 15:50:22 +0800

    Add files via upload
    PHP Luqman Pizza Project

---
+++ tuts/index.php
@@ -0,0 +1,67 @@
+<?php
+
+include('config/db_connect.php');
+
+// write query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+// make query and get results
+$result = mysqli_query($conn, $sql);
+
+// fetch the resulting rows as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// free result from memory
+mysqli_free_result($result);
+
+// close connection
+mysqli_close($conn);
+
+// explode(',', $pizzas[0]['ingredients']);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<?php include('templates/header.php'); ?>
+
+<h4 class="center grey-text">Pizzas!</h4>
+
+<div class="container">
+    <div class="row">
+        <?php foreach ($pizzas as $pizza) : ?>
+
+            <div class="col s6 md3 ">
+                <div class="card z-depth-0">
+                    <img src="img/pizza.svg" class="pizza">
+                    <div class="card-content center">
+                        <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
+                        <ul>
+                            <?php foreach (explode(',', $pizza['ingredients']) as $ing) : ?>
+                                <li><?php echo htmlspecialchars($ing); ?></li>
+                            <?php endforeach; ?>
+                        </ul>
+                    </div>
+                    <div class="card-action right-align">
+                        <a class="brand-text" href="details.php?id=<?php echo $pizza['id'] ?>">more info</a>
+                    </div>
+                </div>
+            </div>
+
+        <?php endforeach; ?>
+
+        <?php if (count($pizzas) >= 3) : ?>
+            <p>there are more than 3 pizzas</p>
+        <?php else : ?>
+            <p>there are less 3 pizzas</p>
+        <?php endif; ?> 
+
+
+
+    </div>
+</div>
+
+<?php include('templates/footer.php'); ?>
+
+
+</html>
